e12c10ccdb328a926a7d6e5ac40d0b2e6dffa2bd
Ali Awari <solojungle@users.noreply.github.com>
2023-11-18 09:28:02 -0500

    prog: changed login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -19,8 +19,8 @@ export default async function AuthenticationPage() {
 	}
 
 	return (
-		<div className="container flex h-screen items-center justify-center bg-black">
-			<div className="flex w-full flex-col items-center justify-center space-y-6 rounded-lg bg-white p-10 sm:max-w-md">
+		<div className="container flex h-screen items-center justify-center bg-background">
+			<div className="flex w-full flex-col items-center justify-center space-y-6 rounded-lg border border-border bg-card p-10 shadow-lg sm:max-w-md">
 				<div className="mb-4 flex flex-col space-y-2 text-center">
 					<span className="flex items-center justify-center pb-10 pt-8 font-semibold">
 						<NotionLogoIcon className="h-36 w-36" />
@@ -35,21 +35,21 @@ export default async function AuthenticationPage() {
 				<UserAuthForm />
 				<Separator />
 				<p className="max-w-xs text-center text-xs text-muted-foreground">
-					By clicking continue, you agree to our{" "}
+					<span>By clicking continue, you agree to our </span>
 					<Link
 						href="/terms"
 						className="underline underline-offset-4 hover:text-primary"
 					>
 						Terms of Service
-					</Link>{" "}
-					and{" "}
+					</Link>
+					<span> and </span>
 					<Link
 						href="/privacy"
 						className="underline underline-offset-4 hover:text-primary"
 					>
 						Privacy Policy
 					</Link>
-					.
+					<span>.</span>
 				</p>
 			</div>
 		</div>
