c65cd71dc2ca233e85bb93b6eed6c2e689a4185b
Ali Awari <solojungle@users.noreply.github.com>
2023-11-18 08:50:24 -0500

    feat: added logout button

--- src/components/navigationbar/userMenu/index.tsx
+++ src/components/navigationbar/userMenu/index.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
@@ -56,7 +60,7 @@ export function UserMenu() {
 					</DropdownMenuItem>
 				</DropdownMenuGroup>
 				<DropdownMenuSeparator />
-				<DropdownMenuItem>
+				<DropdownMenuItem onClick={() => signOut()}>
 					Log out
 					<DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
 				</DropdownMenuItem>
