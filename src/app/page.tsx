a7c9dea6e83c20cab25d6b2ca6fa1bc158955c21
Ali Awari <solojungle@users.noreply.github.com>
2023-11-19 04:56:35 -0500

    feat: removed log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,8 +27,6 @@ export default async function Home() {
 	// const hello = await api.post.hello.query({ text: "from tRPC" });
 	const session = await getServerAuthSession();
 
-	console.log(session);
-
 	if (!session) {
 		return (
 			<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
