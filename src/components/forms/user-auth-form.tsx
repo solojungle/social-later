f3dce2dda3b7b06e3b487fb85104c9fe00ca68ef
Ali Awari <solojungle@users.noreply.github.com>
2023-11-19 04:22:39 -0500

    feat: callback url will now correctly work (might want to check for hijacking urls)

--- src/components/forms/user-auth-form.tsx
+++ src/components/forms/user-auth-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
 import { signIn } from "next-auth/react";
 import * as React from "react";
 
@@ -9,9 +10,21 @@ import { cn } from "@/lib/utils";
 type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement>;
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+	const searchParams = useSearchParams();
+
+	const callbackUrl = searchParams.get("callbackUrl");
+
 	return (
 		<div className={cn("grid w-full", className)} {...props}>
-			<ContinueWithGoogle type="button" onClick={() => signIn("google")} />
+			<ContinueWithGoogle
+				type="button"
+				onClick={() => {
+					// Include callbackUrl if it exists
+					const signInOptions = callbackUrl ? { callbackUrl } : undefined;
+
+					signIn("google", signInOptions);
+				}}
+			/>
 		</div>
 	);
 }
