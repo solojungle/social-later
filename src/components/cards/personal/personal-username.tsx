833393f281402e37bcc08736947e1f1a316f61e5
Ali Awari <solojungle@users.noreply.github.com>
2023-11-25 18:04:23 -0500

    feat: added person username

--- src/components/cards/personal/personal-username.tsx
+++ src/components/cards/personal/personal-username.tsx
@@ -1,62 +1,68 @@
 "use client";
 
-import { Control } from "react-hook-form";
-import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 import {
+	Form,
 	FormControl,
 	FormField,
 	FormItem,
 	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+import { UserSchema, UserSchemaValues } from "@/schemas/user-schema";
 import { useUserStore } from "@/stores/user";
 
 import { SettingsCardBase } from "../settings-card-base";
 
-export const PersonalUsernameFormSchema = z.object({
-	url: z
-		.string()
-		.min(1, {
-			message: "URL must be at least 1 character.",
-		})
-		.max(48, {
-			message: "URL must not be longer than 48 characters.",
-		}),
-});
-
-interface PersonalUsernameCardProps {
-	formControl: Control<any, any>;
-}
-
-export function PersonalUsernameCard({
-	formControl,
-}: PersonalUsernameCardProps) {
+export function PersonalUsernameCard() {
 	const { url } = useUserStore();
 
+	const defaultValues = {
+		url,
+	};
+
+	const form = useForm<UserSchemaValues>({
+		resolver: zodResolver(UserSchema.pick({ url: true })),
+		defaultValues,
+	});
+
+	function onSubmit(data: UserSchemaValues) {
+		toast({
+			title: "You submitted the following values:",
+			description: (
+				<pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+					<code className="text-white">{JSON.stringify(data, null, 2)}</code>
+				</pre>
+			),
+		});
+	}
+
 	return (
-		<SettingsCardBase
-			title="Username"
-			description="Your username acts also acts as your URL namespace."
-			footerSubtitle="Please use 48 characters at maximum."
-			content={
-				<FormField
-					control={formControl}
-					name="url"
-					render={({ field }) => (
-						<FormItem className="w-full">
-							<FormControl>
-								<Input
-									placeholder="Your username"
-									{...field}
-									defaultValue={url}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+				<SettingsCardBase
+					title="Username"
+					description="Your username acts also acts as your URL namespace."
+					footerSubtitle="Please use 48 characters at maximum."
+					content={
+						<FormField
+							control={form.control}
+							name="url"
+							render={({ field }) => (
+								<FormItem className="w-full">
+									<FormControl>
+										<Input placeholder="Your username" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					}
 				/>
-			}
-		/>
+			</form>
+		</Form>
 	);
 }
--- src/components/forms/account-form.tsx
+++ src/components/forms/account-form.tsx
@@ -1,12 +1,13 @@
 import { PersonalAvatarCard } from "../cards/personal/personal-avatar";
 import { PersonalNameCard } from "../cards/personal/personal-name";
+import { PersonalUsernameCard } from "../cards/personal/personal-username";
 
 export function AccountForm() {
 	return (
 		<>
 			<PersonalAvatarCard />
 			<PersonalNameCard />
-			{/* <PersonalUsernameCard formControl={form.control} /> */}
+			<PersonalUsernameCard />
 		</>
 	);
 }
