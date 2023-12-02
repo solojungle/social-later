6f12eefd19fd9e86e2d1c38aef31d6cfc3ae1b9e
Ali Awari <solojungle@users.noreply.github.com>
2023-12-02 18:00:51 -0500

    fixed: uber long names in team switcher now truncates

--- src/components/navigationbar/teamSwitcher/trigger/index.tsx
+++ src/components/navigationbar/teamSwitcher/trigger/index.tsx
@@ -28,16 +28,15 @@ export default function TeamSwitcherPopoverTrigger({
 				role="combobox"
 				aria-expanded={open}
 				aria-label="Select a team"
-				className={cn(
-					"w-[200px] justify-between overflow-hidden truncate",
-					className,
-				)}
+				className={cn("w-[200px] justify-between", className)}
 			>
 				<Avatar className="mr-2 h-5 w-5">
 					<AvatarImage src={selectedTeamImage} alt={selectedTeamName} />
 					<AvatarFallback>{selectedTeamImageFallbackInitials}</AvatarFallback>
 				</Avatar>
-				{selectedTeamName}
+				<span title={selectedTeamName} className="overflow-hidden truncate">
+					{selectedTeamName}
+				</span>
 				<CaretSortIcon className="ml-auto h-4 w-4 shrink-0 opacity-50" />
 			</Button>
 		</PopoverTrigger>
