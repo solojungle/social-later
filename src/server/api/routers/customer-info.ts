c07c1b16eef8ce9a7f1ce2aeff08eeddb388c5cc
Ali Awari <solojungle@users.noreply.github.com>
2023-12-06 18:15:24 -0500

    feat: added del customer from stripe

--- src/server/api/routers/customer-info.ts
+++ src/server/api/routers/customer-info.ts
@@ -35,4 +35,47 @@ export const customerInfoRouter = createTRPCRouter({
 				},
 			});
 		}),
+
+	delete: protectedProcedure
+		.input(CustomerInfoSchema.pick({ teamId: true }))
+		.mutation(async ({ ctx, input }) => {
+			// Check if the user is part of the team
+			const isUserPartOfTeam = await ctx.db.userOnTeam.findFirst({
+				where: {
+					teamId: input.teamId,
+					userId: ctx.session.user.id,
+				},
+			});
+
+			if (!isUserPartOfTeam) {
+				throw new Error("You are not apart of this team");
+			}
+
+			// Check if the user is the owner of the team
+			const isUserOwnerOfTeam = isUserPartOfTeam.role === "OWNER";
+			if (!isUserOwnerOfTeam) {
+				throw new Error("You are not an owner of this team");
+			}
+
+			// Get the customer info
+			const customerInfo = await ctx.db.customerInfo.findFirst({
+				where: {
+					teamId: input.teamId,
+				},
+			});
+
+			if (!customerInfo || !customerInfo.stripeCustomerId) {
+				throw new Error("No customer info found");
+			}
+
+			// Delete the customer from Stripe
+			await stripe.customers.del(customerInfo.stripeCustomerId);
+
+			// Delete the customer info
+			await ctx.db.customerInfo.delete({
+				where: {
+					id: customerInfo.id,
+				},
+			});
+		}),
 });
