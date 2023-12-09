6c73a33d3f4091ec3ef8d159f2bfae18a8b4b42a
Ali Awari <solojungle@users.noreply.github.com>
2023-12-09 11:55:13 -0500

    prog: removed unused billing schemas

--- src/components/forms/billing-form.tsx
+++ src/components/forms/billing-form.tsx
@@ -1,71 +1,11 @@
-"use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-
-import { Form } from "@/components/ui/form";
-import { toast } from "@/components/ui/use-toast";
-
 import { TeamPaymentMethodCard } from "../cards/teams/team-payment-method";
 import { TeamPaymentPlanCard } from "../cards/teams/team-plan";
 
-const BillingFormSchema = z.object({
-	name: z
-		.string()
-		.min(2, {
-			message: "Name must be at least 2 characters.",
-		})
-		.max(30, {
-			message: "Name must not be longer than 30 characters.",
-		}),
-	url: z
-		.string()
-		.min(2, {
-			message: "URL must be at least 2 characters.",
-		})
-		.max(30, {
-			message: "URL must not be longer than 30 characters.",
-		}),
-	dob: z.date({
-		required_error: "A date of birth is required.",
-	}),
-	language: z.string({
-		required_error: "Please select a language.",
-	}),
-});
-
-type BillingFormValues = z.infer<typeof BillingFormSchema>;
-
-// This can come from your database or API.
-const defaultValues: Partial<BillingFormValues> = {
-	// name: "Your name",
-	// dob: new Date("2023-01-23"),
-};
-
 export function BillingForm() {
-	const form = useForm<BillingFormValues>({
-		resolver: zodResolver(BillingFormSchema),
-		defaultValues,
-	});
-
-	function onSubmit(data: BillingFormValues) {
-		toast({
-			title: "You submitted the following values:",
-			description: (
-				<pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-					<code className="text-white">{JSON.stringify(data, null, 2)}</code>
-				</pre>
-			),
-		});
-	}
-
 	return (
-		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-				<TeamPaymentMethodCard />
-				<TeamPaymentPlanCard />
-			</form>
-		</Form>
+		<>
+			<TeamPaymentMethodCard />
+			<TeamPaymentPlanCard />
+		</>
 	);
 }
