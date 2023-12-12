c29843cb560eb06c5d2fa84894fdf89bc1bc770a
Ali Awari <solojungle@users.noreply.github.com>
2023-12-11 23:10:46 -0500

    fix: mobile was hidden

--- src/app/(application)/publish/page.tsx
+++ src/app/(application)/publish/page.tsx
@@ -22,7 +22,7 @@ import { Separator } from "@/components/ui/separator";
 export default async function PublishPage() {
 	return (
 		<main className="">
-			<div className="hidden space-y-6 p-10 pb-16 md:block">
+			<div className="space-y-6 p-10 pb-16 md:block">
 				<div className="space-y-0.5">
 					<h2 className="text-2xl font-bold tracking-tight">Publish</h2>
 					<p className="text-muted-foreground">
