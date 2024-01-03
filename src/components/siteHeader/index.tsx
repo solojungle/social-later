13bcf70c07bf605ae7a1ac25f1a96ddead817799
Ali Awari <solojungle@users.noreply.github.com>
2024-01-02 23:46:19 -0500

    feat: fixing site header

--- src/components/siteHeader/index.tsx
+++ src/components/siteHeader/index.tsx
@@ -1,40 +1,40 @@
+"use client";
+
+import { api } from "@/trpc/react";
+
 import { NavigationBar } from "../navigationbar";
 import { StoreInitializer } from "../storeInitializer";
 
-export async function SiteHeader() {
-	// const userData = await api.user.getUser.query();
-	// const teamsData = await api.user.getTeams.query();
+export function SiteHeader() {
+	const { data: userData } = api.user.getUser.useQuery();
+	const { data: teamsData } = api.user.getTeams.useQuery();
 
-	// const teamsArray = teamsData.map((team) => team.team);
+	if (!userData || !teamsData) {
+		return null;
+	}
+
+	const teamsArray = teamsData.map((team) => team.team);
 
 	// Now create a new teams array that has the properties: type, and imageFallbackInitials
-	// const teamsArrayWithProperties = teamsArray.map((team) => {
-	// 	return {
-	// 		...team,
-	// 		type: "team",
-	// 		imageFallbackInitials: "TT",
-	// 	};
-	// });
+	const teamsArrayWithProperties = teamsArray.map((team) => {
+		return {
+			...team,
+			type: "team",
+			imageFallbackInitials: "TT",
+		};
+	});
 
-	// const userWithProperties = {
-	// 	...userData,
-	// 	type: "personal",
-	// 	imageFallbackInitials: "AA",
-	// };
+	const userWithProperties = {
+		...userData,
+		type: "personal",
+		imageFallbackInitials: "AA",
+	};
 
 	return (
 		<header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
 			<StoreInitializer
-				user={{
-					id: "1",
-					name: "John",
-					image: "",
-					url: "",
-					type: "personal",
-					imageFallbackInitials: "AA",
-					email: "",
-				}}
-				teams={[]}
+				user={{ ...userWithProperties }}
+				teams={teamsArrayWithProperties}
 			/>
 			<NavigationBar />
 		</header>
