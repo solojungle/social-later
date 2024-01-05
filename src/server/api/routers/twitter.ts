be9a1d368ded6ee10c47d41606d21499b84f1623
Ali Awari <solojungle@users.noreply.github.com>
2024-01-05 15:31:15 -0500

    feat: twitter auth now will connect

--- src/server/api/routers/twitter.ts
+++ src/server/api/routers/twitter.ts
@@ -1,25 +1,26 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
-import { twitter, twitterUserOAuth } from "@/server/services/twitter/client";
+import { twitterUserOAuth } from "@/server/services/twitter/client";
 
 export const twitterRouter = createTRPCRouter({
 	getPosts: protectedProcedure.query(async () => {}),
-	createPost: protectedProcedure.mutation(async () => {
-		try {
-			const resp = await twitter.tweets.createTweet({
-				text: "Hello World",
-			});
-			return resp;
-		} catch (error) {
-			console.log(error);
-		}
+	// createPost: protectedProcedure.mutation(async () => {
+	// 	try {
+	// 		const resp = await twitter.tweets.createTweet({
+	// 			text: "Hello World",
+	// 		});
+	// 		return resp;
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
 
-		return null;
-	}),
+	// 	return null;
+	// }),
 
-	generateOAuth2URL: protectedProcedure.query(async () => {
+	generateOAuth2URL: protectedProcedure.query(() => {
 		return twitterUserOAuth.generateAuthURL({
-			state: "B7D674278FDE4316FA91D1A7947C3BCDC417E193D324B47B11B46618B3",
-			code_challenge_method: "s256",
+			state: "my-state",
+			code_challenge_method: "plain",
+			code_challenge: "test",
 		});
 	}),
 });
--- src/server/services/twitter/client.ts
+++ src/server/services/twitter/client.ts
@@ -1,13 +1,10 @@
-import { auth, Client } from "twitter-api-sdk";
+import { auth } from "twitter-api-sdk";
 
 import { env } from "@/env.mjs";
-
-export const twitter = new Client(env.TWITTER_BEARER_TOKEN);
-export const twitterAuth = new auth.OAuth2Bearer(env.TWITTER_BEARER_TOKEN);
 
 export const twitterUserOAuth = new auth.OAuth2User({
 	client_id: env.TWITTER_CLIENT_ID,
 	client_secret: env.TWITTER_CLIENT_SECRET,
 	callback: `${env.TWITTER_CALLBACK_URL}/api/webhooks/twitter/callback`,
-	scopes: ["tweet.read", "tweet.write", "users.read", "offline.access"],
+	scopes: ["tweet.read", "users.read", "offline.access", "tweet.write"],
 });
