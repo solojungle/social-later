81874940116331ac65397bf392e5991ef773327a
Ali Awari <solojungle@users.noreply.github.com>
2024-01-10 19:12:51 -0500

    feat: fixed nav to collapse

--- src/components/resizableLayout/index.tsx
+++ src/components/resizableLayout/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 import {
 	AlertCircle,
 	Archive,
@@ -39,108 +40,98 @@ export function ResizableLayout({
 	const [isCollapsed, setIsCollapsed] = useState<boolean>(defaultCollapsed);
 
 	return (
-		<ResizablePanelGroup direction="horizontal">
-			<ResizablePanel
-				defaultSize={defaultLayout[0]}
-				collapsedSize={navCollapsedSize}
-				collapsible
-				minSize={15}
-				maxSize={20}
-				onCollapse={(collapsed: boolean) => {
-					setIsCollapsed(collapsed);
-					document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
-						collapsed,
-					)}`;
-				}}
-				className={cn(
-					isCollapsed && "min-w-[50px] transition-all duration-300 ease-in-out",
-				)}
-			>
-				<div
+		<TooltipProvider delayDuration={0}>
+			<ResizablePanelGroup direction="horizontal">
+				<ResizablePanel
+					defaultSize={defaultLayout[0]}
+					collapsedSize={navCollapsedSize}
+					collapsible
+					minSize={15}
+					maxSize={20}
+					onCollapse={() => setIsCollapsed(true)}
+					onExpand={() => setIsCollapsed(false)}
 					className={cn(
-						"flex h-[52px] items-center justify-center",
-						isCollapsed ? "h-[52px]" : "px-2",
+						isCollapsed &&
+							"min-w-[50px] transition-all duration-300 ease-in-out",
 					)}
 				>
-					{/* <AccountSwitcher isCollapsed={isCollapsed} accounts={accounts} /> */}
-				</div>
-				<Separator />
-				<Nav
-					isCollapsed={isCollapsed}
-					links={[
-						{
-							title: "Inbox",
-							label: "128",
-							icon: Inbox,
-							variant: "default",
-						},
-						{
-							title: "Sent",
-							label: "",
-							icon: Send,
-							variant: "ghost",
-						},
-						{
-							title: "Junk",
-							label: "23",
-							icon: ArchiveX,
-							variant: "ghost",
-						},
-						{
-							title: "Trash",
-							label: "",
-							icon: Trash2,
-							variant: "ghost",
-						},
-						{
-							title: "Archive",
-							label: "",
-							icon: Archive,
-							variant: "ghost",
-						},
-					]}
-				/>
-				<Separator />
-				<Nav
-					isCollapsed={isCollapsed}
-					links={[
-						{
-							title: "Social",
-							label: "972",
-							icon: Users2,
-							variant: "ghost",
-						},
-						{
-							title: "Updates",
-							label: "342",
-							icon: AlertCircle,
-							variant: "ghost",
-						},
-						{
-							title: "Forums",
-							label: "128",
-							icon: MessagesSquare,
-							variant: "ghost",
-						},
-						{
-							title: "Shopping",
-							label: "8",
-							icon: ShoppingCart,
-							variant: "ghost",
-						},
-						{
-							title: "Promotions",
-							label: "21",
-							icon: Archive,
-							variant: "ghost",
-						},
-					]}
-				/>
-			</ResizablePanel>
-			<ResizableHandle withHandle />
-			<ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
-				{children}
-			</ResizablePanel>
-		</ResizablePanelGroup>
+					<Nav
+						isCollapsed={isCollapsed}
+						links={[
+							{
+								title: "Inbox",
+								label: "128",
+								icon: Inbox,
+								variant: "default",
+							},
+							{
+								title: "Sent",
+								label: "",
+								icon: Send,
+								variant: "ghost",
+							},
+							{
+								title: "Junk",
+								label: "23",
+								icon: ArchiveX,
+								variant: "ghost",
+							},
+							{
+								title: "Trash",
+								label: "",
+								icon: Trash2,
+								variant: "ghost",
+							},
+							{
+								title: "Archive",
+								label: "",
+								icon: Archive,
+								variant: "ghost",
+							},
+						]}
+					/>
+					<Separator />
+					<Nav
+						isCollapsed={isCollapsed}
+						links={[
+							{
+								title: "Social",
+								label: "972",
+								icon: Users2,
+								variant: "ghost",
+							},
+							{
+								title: "Updates",
+								label: "342",
+								icon: AlertCircle,
+								variant: "ghost",
+							},
+							{
+								title: "Forums",
+								label: "128",
+								icon: MessagesSquare,
+								variant: "ghost",
+							},
+							{
+								title: "Shopping",
+								label: "8",
+								icon: ShoppingCart,
+								variant: "ghost",
+							},
+							{
+								title: "Promotions",
+								label: "21",
+								icon: Archive,
+								variant: "ghost",
+							},
+						]}
+					/>
+				</ResizablePanel>
+				<ResizableHandle withHandle />
+				<ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
+					{children}
+				</ResizablePanel>
+			</ResizablePanelGroup>
+		</TooltipProvider>
 	);
 }
