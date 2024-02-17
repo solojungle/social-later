c1cf1d06c9fc32e60769d8c582af8acb059e789e
Ali Awari <solojungle@users.noreply.github.com>
2024-02-17 17:44:34 -0500

    feat: removed product images

--- src/components/navigationbar/teamSwitcher/modal/index.tsx
+++ src/components/navigationbar/teamSwitcher/modal/index.tsx
@@ -70,11 +70,11 @@ function ProductsSelector({ products, field }: ProductsSelectorProps) {
 							htmlFor={product.id}
 							className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
 						>
-							<img
+							{/* <img
 								alt={product.name}
 								src={product.image}
-								className="mb-3 h-7 w-7"
-							/>
+								className="mb-3 h-9 w-9"
+							/> */}
 							<span className="mb-2">{product.name}</span>
 							<span className="text-xs text-muted-foreground">
 								{product.priceFormatted} per month
