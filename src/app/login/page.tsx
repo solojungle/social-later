e1adb81c10a076e5e39b7fbe6a3ebc41c302cf3a
Ali Awari <solojungle@users.noreply.github.com>
2024-03-02 05:19:50 -0500

    feat: added back button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
+import BackButton from "@/components/backButton";
 import { UserAuthForm } from "@/components/forms/user-auth-form";
 import { getServerAuthSession } from "@/server/auth";
 
@@ -19,7 +20,7 @@ export default async function AuthenticationPage() {
 
 	return (
 		<div className="h-screen">
-			<div className="container relative grid h-[800px] flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+			<div className="container relative grid h-full flex-col items-center justify-center p-10 lg:max-w-none lg:grid-cols-2 lg:p-0">
 				<div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
 					<div className="absolute inset-0 h-screen bg-zinc-900" />
 					<div className="relative z-20 flex items-center text-lg font-medium">
@@ -48,8 +49,9 @@ export default async function AuthenticationPage() {
 						</blockquote>
 					</div>
 				</div>
-				<div className="lg:p-8">
-					<div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+				<div className="h-full lg:p-8">
+					<BackButton />
+					<div className="mx-auto flex h-full w-full grow flex-col justify-center space-y-6 sm:w-[350px]">
 						<div className="mb-8 flex flex-col space-y-2 text-start">
 							<h1 className="text-2xl font-semibold tracking-tight">
 								Welcome back. Sign into your account below.
