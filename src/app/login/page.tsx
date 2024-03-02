2150ad684c8e603fbb6b040f5e08b96b5e223dfd
Ali Awari <solojungle@users.noreply.github.com>
2024-03-02 05:00:42 -0500

    feat: updated login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,63 +1,79 @@
-import { NotionLogoIcon } from "@radix-ui/react-icons";
-import { type Metadata } from "next";
+import { Metadata } from "next";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
-import BackButton from "@/components/backButton";
 import { UserAuthForm } from "@/components/forms/user-auth-form";
-import { Separator } from "@/components/ui/separator";
 import { getServerAuthSession } from "@/server/auth";
 
 export const metadata: Metadata = {
 	title: "Authentication",
-	description: "Authentication forms built using the components.",
+	description: "Sign in to your account to continue.",
 };
 
 export default async function AuthenticationPage() {
 	const session = await getServerAuthSession();
 
-	// const session = await getServerAuthSession();
 	if (session) {
 		redirect("/publish");
 	}
 
 	return (
-		<div className="flex h-screen flex-col">
-			<div className="container py-5 md:py-10">
-				<BackButton />
-			</div>
-			<div className="container flex items-center justify-center bg-background">
-				<div className="flex w-full flex-col items-center justify-center space-y-6 rounded-lg border border-border bg-card p-10 shadow-lg sm:max-w-md">
-					<div className="mb-4 flex flex-col space-y-2 text-center">
-						<span className="flex items-center justify-center pb-10 pt-8 font-semibold">
-							<NotionLogoIcon className="h-36 w-36" />
-						</span>
-						<h1 className="text-2xl font-semibold tracking-tight">
-							Client Login
-						</h1>
-						<p className="text-sm text-muted-foreground">
-							Welcome back. Sign into your account below.
+		<div className="h-screen">
+			<div className="container relative grid h-[800px] flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+				<div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+					<div className="absolute inset-0 h-screen bg-zinc-900" />
+					<div className="relative z-20 flex items-center text-lg font-medium">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							className="mr-2 h-6 w-6"
+						>
+							<path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+						</svg>
+						Social Later
+					</div>
+					<div className="relative z-20 mt-auto">
+						<blockquote className="space-y-2">
+							<p className="text-lg">
+								&ldquo;Its been a game-changer for our agency, increasing our
+								efficiency and enabling us to elevate our social media presence
+								to new heights.&rdquo;
+							</p>
+							<footer className="text-sm">Ali Awari</footer>
+						</blockquote>
+					</div>
+				</div>
+				<div className="lg:p-8">
+					<div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+						<div className="mb-8 flex flex-col space-y-2 text-start">
+							<h1 className="text-2xl font-semibold tracking-tight">
+								Welcome back. Sign into your account below.
+							</h1>
+						</div>
+						<UserAuthForm />
+						<p className="px-8 text-center text-xs text-muted-foreground">
+							<span>By clicking continue, you agree to our </span>
+							<Link
+								href="/terms"
+								className="underline underline-offset-4 hover:text-primary"
+							>
+								Terms of Service
+							</Link>
+							<span> and </span>
+							<Link
+								href="/privacy"
+								className="underline underline-offset-4 hover:text-primary"
+							>
+								Privacy Policy
+							</Link>
+							.
 						</p>
 					</div>
-					<UserAuthForm />
-					<Separator />
-					<p className="max-w-xs text-center text-xs text-muted-foreground">
-						<span>By clicking continue, you agree to our </span>
-						<Link
-							href="/terms"
-							className="underline underline-offset-4 hover:text-primary"
-						>
-							Terms of Service
-						</Link>
-						<span> and </span>
-						<Link
-							href="/privacy"
-							className="underline underline-offset-4 hover:text-primary"
-						>
-							Privacy Policy
-						</Link>
-						<span>.</span>
-					</p>
 				</div>
 			</div>
 		</div>
