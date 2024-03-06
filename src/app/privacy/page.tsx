f5ed190af884cfc86bba252850cdc32025cb9992
Ali Awari <solojungle@users.noreply.github.com>
2024-03-06 13:39:47 -0500

    feat: added privacy and terms pages

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,3 @@
+export default function PrivacyPolicyPage() {
+	return <div className="space-y-6">Privacy policy</div>;
+}
---
+++ src/app/terms/page.tsx
@@ -0,0 +1,3 @@
+export default function TermsAndConditionsPage() {
+	return <div className="space-y-6">Terms and Conditions</div>;
+}
