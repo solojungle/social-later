05017775728883c31ae33cd6a722e0437515c0a0
Ali Awari <solojungle@users.noreply.github.com>
2024-03-28 06:28:05 -0400

    feat: added logo

--- src/landing/navigationMenu/index.tsx
+++ src/landing/navigationMenu/index.tsx
@@ -10,8 +10,13 @@ export function NavigationMenu() {
 	return (
 		<div className="flex h-16 w-full max-w-7xl items-center justify-between px-4 py-2">
 			<div className="flex items-center">
-				<a href="https://feedfrenzy.co/">
-					<span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+				<a href="https://feedfrenzy.co/" className="flex">
+					<img
+						alt="feedfrenzy logo"
+						src="/images/logo.png"
+						className="mr-3 h-8 w-8 rounded-sm"
+					/>
+					<span className="self-center whitespace-nowrap text-base font-semibold dark:text-white">
 						feedfrenzy.co
 					</span>
 				</a>
