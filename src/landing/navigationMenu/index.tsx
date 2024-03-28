4159c653c40456c25b0d765805d5aefaf1d3737c
Ali Awari <solojungle@users.noreply.github.com>
2024-03-28 15:00:58 -0400

    feat: adding hamburger menu to nav

--- src/landing/navigationMenu/index.tsx
+++ src/landing/navigationMenu/index.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { ArrowUpRight } from "lucide-react";
+import { ArrowUpRight, MenuIcon, XIcon } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 import { Button, buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 export function NavigationMenu() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false); // State to track menu open/close
+
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen); // Toggle menu state
+	};
+
 	return (
 		<div className="flex h-16 w-full max-w-7xl items-center justify-between px-4 py-2">
 			<div className="flex items-center">
@@ -21,7 +28,24 @@ export function NavigationMenu() {
 					</span>
 				</a>
 			</div>
-			<div className="flex space-x-4">
+			{/* Hamburger Menu */}
+			<div className="flex space-x-4 md:hidden">
+				<Button
+					type="button"
+					variant="default"
+					size="icon"
+					aria-label="Toggle Menu"
+					onClick={toggleMenu} // Toggle menu on click
+				>
+					{isMenuOpen ? (
+						<XIcon className="h-6 w-6" /> // Show close icon when menu is open
+					) : (
+						<MenuIcon className="h-6 w-6" /> // Show hamburger icon otherwise
+					)}
+				</Button>
+			</div>
+			{/* Desktop Menu */}
+			<div className="hidden space-x-4 md:flex">
 				<Link
 					href="/login"
 					className={cn(
@@ -31,7 +55,7 @@ export function NavigationMenu() {
 				>
 					<span>Login</span>
 				</Link>
-				<Button variant="outline">
+				<Button variant="default">
 					Start a free trial <ArrowUpRight className="ml-1 h-5 w-5" />
 				</Button>
 			</div>
