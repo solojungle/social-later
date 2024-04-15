e0743adbfdadf6bb314482f9b32af836195600d4
Ali Awari <solojungle@users.noreply.github.com>
2024-04-15 04:13:14 -0400

    feat: working discord callback (no spam protection)

--- src/app/api/webhooks/discord/route.ts
+++ src/app/api/webhooks/discord/route.ts
@@ -1,18 +1,7 @@
 // When this route is hit, it will send a message to the Discord channel. Saying that a user has signed up.
-
-import { NextRequest } from "next/server";
-
-export async function POST(req: NextRequest) {
+export async function POST() {
 	const discordWebhookUrl =
 		"https://discord.com/api/webhooks/1229337363197726730/ZWRDCPTYgKt11KU0ETPUI6Q0i6YTQ6ea0humJk4djfnQgtfiHwaK4wjb4czAWjHOrp3-";
-
-	const body = await req.text();
-
-	if (body === null) {
-		return new Response("Bad request", {
-			status: 400,
-		});
-	}
 
 	await fetch(discordWebhookUrl, {
 		method: "POST",
@@ -20,7 +9,7 @@ export async function POST(req: NextRequest) {
 			"Content-Type": "application/json",
 		},
 		body: JSON.stringify({
-			content: `A user has signed up!`,
+			content: `@here A user has signed up!`,
 		}),
 	});
 
