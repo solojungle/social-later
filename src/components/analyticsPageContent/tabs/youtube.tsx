856b0e1fe008d924aeae5bbce33bb8598bbaec36
Ali Awari <solojungle@users.noreply.github.com>
2024-06-09 17:22:33 -0400

    feat: added loading spinner

--- src/components/analyticsPageContent/tabs/youtube.tsx
+++ src/components/analyticsPageContent/tabs/youtube.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { ArrowDownRight, ArrowUpRight, InfoIcon, Minus } from "lucide-react";
+import {
+	ArrowDownRight,
+	ArrowUpRight,
+	InfoIcon,
+	Loader2,
+	Minus,
+} from "lucide-react";
 
 import {
 	Tooltip,
@@ -188,7 +194,11 @@ export const YouTubeAnalyticsTab = () => {
 	// 	});
 
 	if (isFetching) {
-		return <p>Loading...</p>;
+		return (
+			<div className="flex h-full items-center justify-center">
+				<Loader2 className="h-16 w-16 animate-spin text-muted-foreground" />
+			</div>
+		);
 	}
 
 	const counts = analytics?.data?.items[0]?.statistics;
