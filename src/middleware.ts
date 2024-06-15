a3e618ed487b7aef43501f97cf6405216ab00455
Ali Awari <solojungle@users.noreply.github.com>
2024-06-15 11:26:03 -0400

    prog: trying to fix prod login

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
-	const path = req.nextUrl.pathname;
+	// const path = req.nextUrl.pathname;
 	const session = !!req.cookies.get("next-auth.session-token");
 
 	if (!session) {
-		return NextResponse.redirect(
-			new URL(`/api/auth/signin?callbackUrl=${path}`, req.url),
-		);
+		return NextResponse.redirect(new URL("login", req.url));
 	}
 	return NextResponse.next();
 }
