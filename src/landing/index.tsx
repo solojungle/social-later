94c9d99dc0afa083b4b1a2e979581ec9d5b6209e
Ali Awari <solojungle@users.noreply.github.com>
2024-06-14 23:12:58 -0400

    feat: make bg changed colour on darkmode for landing

--- src/landing/index.tsx
+++ src/landing/index.tsx
@@ -3,7 +3,11 @@ import { NavigationMenu } from "./navigationMenu";
 
 export default async function LandingPage() {
 	return (
-		<main className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,orange)]">
+		<main
+			className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,orange)]
+			dark:[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,orange)]
+		"
+		>
 			<div className="flex w-full flex-col justify-center">
 				<NavigationMenu />
 				<Hero />
