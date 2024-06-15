4dca786a1fd3d615c7389326f58aef4ee6846da9
Ali Awari <solojungle@users.noreply.github.com>
2024-06-15 12:03:09 -0400

    feat: turnoff middleware actually

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,16 @@
-// middleware.ts
-import { NextRequest, NextResponse } from "next/server";
+// // middleware.ts
+// import { NextRequest, NextResponse } from "next/server";
 
-export default async function middleware(req: NextRequest) {
-	// const path = req.nextUrl.pathname;
-	const session = !!req.cookies.get("next-auth.session-token");
+// export default async function middleware(req: NextRequest) {
+// 	// const path = req.nextUrl.pathname;
+// 	const session = !!req.cookies.get("next-auth.session-token");
 
-	if (!session) {
-		return NextResponse.redirect(new URL("login", req.url));
-	}
-	return NextResponse.next();
-}
+// 	if (!session) {
+// 		return NextResponse.redirect(new URL("login", req.url));
+// 	}
+// 	return NextResponse.next();
+// }
 
-export const config = {
-	// matcher: ["/publish", "/analytics", "/settings/:path*", "/teams/:path*"],
-};
+// export const config = {
+// 	// matcher: ["/publish", "/analytics", "/settings/:path*", "/teams/:path*"],
+// };
