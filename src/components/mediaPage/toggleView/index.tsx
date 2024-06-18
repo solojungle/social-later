e3c2cf5dbebecf64ce2bb56e94d3f744eaf10900
Ali Awari <solojungle@users.noreply.github.com>
2024-06-18 12:54:34 -0400

    feat: upgraded lucide icons

---
+++ src/components/mediaPage/toggleView/index.tsx
@@ -0,0 +1,10 @@
+import { LayoutGridIcon, Rows3Icon } from "lucide-react";
+
+export function ToggleView() {
+	return (
+		<div className="flex items-center space-x-2">
+			<LayoutGridIcon />
+			<Rows3Icon />
+		</div>
+	);
+}
