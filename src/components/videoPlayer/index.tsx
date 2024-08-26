132a98eba55924b8d3442a0d7c72e14eb2757bb8
Ali Awari <solojungle@users.noreply.github.com>
2024-08-26 13:38:40 -0400

    feat: added video player components

--- src/components/videoPlayer/index.tsx
+++ src/components/videoPlayer/index.tsx
@@ -1,16 +1,84 @@
 import "@vidstack/react/player/styles/base.css";
 
-import { MediaPlayer, MediaProvider, Poster } from "@vidstack/react";
+import {
+	isHLSProvider,
+	type MediaCanPlayDetail,
+	type MediaCanPlayEvent,
+	MediaPlayer,
+	type MediaPlayerInstance,
+	MediaProvider,
+	type MediaProviderAdapter,
+	type MediaProviderChangeEvent,
+	Poster,
+} from "@vidstack/react";
+import { useEffect, useRef } from "react";
 
-export const VideoPlayer = () => {
+import { VideoLayout } from "./components/layouts/video-layout";
+
+type PlayerProps = {
+	title: string;
+	video: string;
+	poster: string;
+	posterAlt: string;
+	thumbnails: string;
+};
+
+export function Player({
+	title,
+	video,
+	poster,
+	posterAlt,
+	thumbnails,
+}: PlayerProps) {
+	const player = useRef<MediaPlayerInstance>(null);
+
+	useEffect(() => {
+		// Subscribe to state updates.
+		return player.current!.subscribe(({ paused, viewType }) => {
+			// console.log('is paused?', '->', state.paused);
+			// console.log('is audio view?', '->', state.viewType === 'audio');
+		});
+	}, []);
+
+	function onProviderChange(
+		provider: MediaProviderAdapter | null,
+		nativeEvent: MediaProviderChangeEvent,
+	) {
+		// We can configure provider's here.
+		if (isHLSProvider(provider)) {
+			provider.config = {};
+		}
+	}
+
+	// We can listen for the `can-play` event to be notified when the player is ready.
+	function onCanPlay(
+		detail: MediaCanPlayDetail,
+		nativeEvent: MediaCanPlayEvent,
+	) {
+		// ...
+	}
+
 	return (
-		<MediaPlayer title="Sprite Fight" src="youtube/_cMxraX_5RE">
+		<MediaPlayer
+			className="ring-media-focus aspect-video w-full overflow-hidden rounded-md bg-slate-900 font-sans text-white data-[focus]:ring-4"
+			title={title}
+			src={video}
+			crossOrigin
+			playsInline
+			// eslint-disable-next-line react/jsx-no-bind
+			onProviderChange={onProviderChange}
+			// eslint-disable-next-line react/jsx-no-bind
+			onCanPlay={onCanPlay}
+			ref={player}
+		>
 			<MediaProvider>
 				<Poster
-					className="absolute inset-0 block h-full w-full rounded-md bg-black opacity-0 transition-opacity data-[visible]:opacity-100 [&>img]:h-full [&>img]:w-full [&>img]:object-cover"
-					src="https://files.vidstack.io/sprite-fight/poster.webp"
+					className="absolute inset-0 block h-full w-full rounded-md object-cover opacity-0 transition-opacity data-[visible]:opacity-100"
+					src={poster}
+					alt={posterAlt}
 				/>
 			</MediaProvider>
+			<VideoLayout thumbnails={thumbnails} />
 		</MediaPlayer>
 	);
-};
+}
