169c5e8321a35374bcec2084a8a56ce95b545157
Ali Awari <solojungle@users.noreply.github.com>
2024-09-13 16:17:55 -0400

    feat: adding threads

--- src/components/addSocialProfileButton/index.tsx
+++ src/components/addSocialProfileButton/index.tsx
@@ -14,6 +14,7 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from "../ui/dialog";
+import { LinkThreadsButton } from "./linkThreadsButton";
 import { LinkYouTubeButton } from "./linkYoutubeButton";
 
 const SupportedSites = [
@@ -51,6 +52,7 @@ export function ProfileCards() {
 			{/* <LinkTwitterButton teamId={teamId} /> */}
 			{/* <LinkLinkedInButton teamId={teamId} /> */}
 			<LinkYouTubeButton teamId={teamId} />
+			<LinkThreadsButton teamId={teamId} />
 			{SupportedSites.map((site) => (
 				<div key={site.name} className="cursor-not-allowed">
 					<span
