a41078d3db596079b29359b9481fd4c988fffe48
Ali Awari <solojungle@users.noreply.github.com>
2024-10-11 19:21:08 -0400

    feat: added new logo, cleaned up logos, working on storage usage

--- src/components/storageUsage/index.tsx
+++ src/components/storageUsage/index.tsx
@@ -1,44 +1,25 @@
-import { HardDrive } from "lucide-react";
-
 import { Progress } from "@/components/ui/progress";
+
+import { InterfaceIcons } from "../ui/icons";
 
 export function PlanUsage() {
 	// For demo purposes, using static values
-	const used = 0.5;
+	const used = 3;
 	const total = 5;
 	const progress = (used / total) * 100;
-	const remaining = total - used;
 
 	return (
-		<div className="rounded-lg border border-border bg-card p-6 transition-all ">
-			<div className="flex items-center gap-3">
-				<div className="rounded-full bg-primary/10 p-2">
-					<HardDrive className="h-5 w-5 text-primary" />
+		<div className="rounded-lg border border-border p-3 transition-all ">
+			<div className="mb-1.5 flex w-64 flex-1 justify-between text-sm">
+				<div className="flex items-center gap-2">
+					<InterfaceIcons.Socials.YouTube className="size-4" />
+					<span className="text-xs font-semibold">YouTube</span>
 				</div>
-				<div className="flex-1">
-					<div className="flex items-center justify-between">
-						<h3 className="font-medium">Storage Usage</h3>
-						<p className="text-sm font-medium">
-							{used} GB / {total} GB
-						</p>
-					</div>
-
-					<div className="mt-4">
-						<Progress value={progress} className="h-2 transition-all" />
-					</div>
-
-					<div className="mt-2 flex justify-between text-sm text-muted-foreground">
-						<div className="flex flex-col">
-							<span>{used} GB</span>
-							<span className="text-xs">Used</span>
-						</div>
-						<div className="flex flex-col text-right">
-							<span>{remaining} GB</span>
-							<span className="text-xs">Remaining</span>
-						</div>
-					</div>
-				</div>
+				<span className="text-xs text-muted-foreground">
+					{used} / {total} Posts
+				</span>
 			</div>
+			<Progress value={progress} className="h-2 transition-all" />
 		</div>
 	);
 }
