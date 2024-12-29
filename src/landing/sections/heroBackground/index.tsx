5ff3acd3a8018136f6df9ce0dcba56839843747f
Ali Awari <solojungle@users.noreply.github.com>
2024-12-28 22:13:05 -0800

    feat: added hero background component

---
+++ src/landing/sections/heroBackground/index.tsx
@@ -0,0 +1,17 @@
+export function HeroBackground() {
+	return (
+		<div className="absolute inset-0 z-0">
+			<img
+				className="block h-[130vh] w-full bg-stone-100 md:h-[170vh]"
+				src="https://cdn.prod.website-files.com/66cc2bd703ccf308a49a6188/66e9eaa5aae0bf000ce55162_hero-bg-light.avif"
+				alt=""
+				data-light-src="https://cdn.prod.website-files.com/66cc2bd703ccf308a49a6188/66e9eaa5aae0bf000ce55162_hero-bg-light.avif"
+				data-dark-src="https://cdn.prod.website-files.com/66cc2bd703ccf308a49a6188/66e9ebfcf14a0bce6e73d5bc_hero-bg-dark.avif"
+				data-dark-srcset="https://cdn.prod.website-files.com/66cc2bd703ccf308a49a6188/66e9ebfcf14a0bce6e73d5bc_hero-bg-dark.avif"
+				data-light-srcset="https://cdn.prod.website-files.com/66cc2bd703ccf308a49a6188/66e9eaa5aae0bf000ce55162_hero-bg-light.avif"
+				loading="lazy"
+				srcSet="https://cdn.prod.website-files.com/66cc2bd703ccf308a49a6188/66e9eaa5aae0bf000ce55162_hero-bg-light.avif"
+			/>
+		</div>
+	);
+}
