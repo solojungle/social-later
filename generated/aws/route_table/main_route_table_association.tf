47145cfd5396fa8339e0b4b855c9396cec555389
Ali Awari <solojungle@users.noreply.github.com>
2025-01-18 23:59:30 -0800

    prog: remove terraform for now

--- generated/aws/route_table/main_route_table_association.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_main_route_table_association" "tfer--vpc-006fe2898f9ea459a" {
-  route_table_id = "${data.terraform_remote_state.route_table.outputs.aws_route_table_tfer--rtb-0dfc6befbe03b4063_id}"
-  vpc_id         = "${data.terraform_remote_state.vpc.outputs.aws_vpc_tfer--vpc-006fe2898f9ea459a_id}"
-}
--- generated/aws/sg/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aws_security_group_tfer--default_sg-0e435326efc4ab7a9_id" {
-  value = "${aws_security_group.tfer--default_sg-0e435326efc4ab7a9.id}"
-}
