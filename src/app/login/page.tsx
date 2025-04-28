428b6a48eb74cb1e71c61d2d0237d09f350c612c
Ali Awari <solojungle@users.noreply.github.com>
2025-04-27 23:13:25 -0700

    prog: added gradient to login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -21,7 +21,13 @@ export default async function AuthenticationPage() {
     <div className="h-screen">
       <div className="container relative grid h-full flex-col items-center justify-center p-10 lg:max-w-none lg:grid-cols-2 lg:p-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 h-screen bg-zinc-900" />
+          <div
+            className="absolute inset-0 h-screen bg-cover bg-center"
+            style={{
+              backgroundImage:
+                "linear-gradient(to bottom, rgba(0,0,0,0.2) 0%, transparent 25%), linear-gradient(to top, rgba(0,0,0,0.6) 0%, transparent 25%), url('/backgrounds/image-mesh-gradient-2.png')",
+            }}
+          />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <img
               alt="logo"
